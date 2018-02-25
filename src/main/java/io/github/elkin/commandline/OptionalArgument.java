7d2c97ba252fcf72a14f1b86adf23c33ffe57425
Vladimir Elkin <elkin.v.n@gmail.com>
2018-02-26 01:16:35 +0300

    Add new way to get all values from the optional argument

--- src/main/java/io/github/elkin/commandline/OptionalArgument.java
+++ src/main/java/io/github/elkin/commandline/OptionalArgument.java
@@ -7,11 +7,20 @@ import java.util.function.Predicate;
 public class OptionalArgument extends Argument {
     private Optional<String> d_value;
     private Values d_remainder;
+    private Values d_values;
 
     OptionalArgument(String name, int position) {
         super(name, false, position);
         d_value = Optional.empty();
         d_remainder = ValuesImpl.empty();
+        d_values = ValuesImpl.empty();
+    }
+
+    @Override
+    public OptionalArgument addDefaultValue(String value)
+    {
+        super.addDefaultValue(value);
+        return this;
     }
 
     @Override
@@ -42,15 +51,18 @@ public class OptionalArgument extends Argument {
         assert !value.isEmpty();
 
         d_value = Optional.of(value);
+        d_values = new ValuesImpl(value);
     }
 
     @Override
     void setRemainder(Values remainder)
     {
+        assert d_value.isPresent();
         assert remainder != null;
         assert !remainder.isEmpty();
 
         d_remainder = remainder;
+        d_values = new ValuesImpl(d_value.get(), d_remainder);
     }
 
     public Optional<String> value()
@@ -61,6 +73,11 @@ public class OptionalArgument extends Argument {
     public Values remainder()
     {
         return d_remainder;
+    }
+
+    public Values values()
+    {
+        return d_values;
     }
 
     @Override
